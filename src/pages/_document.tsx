92b97190e5cf254fa873738a3a9c2a10c0940cad
Saito Yuki <phicyanoc@gmail.com>
2022-08-16 10:38:52 +0900

    Custom Documentを追加

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+const Document = () => {
+  return (
+    <Html lang="ja-JP">
+      <Head>
+        <meta name="application-name" content="MyTemplate" />
+        <meta name="desctiption content=" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
