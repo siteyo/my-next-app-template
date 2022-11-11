52a573d4235a3da9cc62a80dadd611abf946a1f5
Saito Yuki <phicyanoc@gmail.com>
2022-11-11 22:51:11 +0900

    Prettierでフォーマット

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
   return (
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
         <h1>My Template</h1>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
